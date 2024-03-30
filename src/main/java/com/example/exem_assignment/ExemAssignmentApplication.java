0675f262fa2e1868c4141f1976aaa304e74fa918
SongJiho1998 <sung7054@naver.com>
2024-03-30 23:42:03 +0900

    커밋 테스트

---
+++ src/main/java/com/example/exem_assignment/ExemAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.exem_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exem_assignment/ExemAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exem_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
